3c39a8c094f7bdcea65cd26ec4243d19805a94ba
Justin Bantuelle <justin@mudbugmedia.com>
2021-11-27 13:04:28 -0600

    Add Zones To Reference Page

---
+++ config/initializers/references/zones.rb
@@ -0,0 +1,276 @@
+ZONES = [
+  {
+    name: "Gate of Guidance",
+    index: 0,
+    preset_coordinates: [
+      {
+        room: 1,
+        screen: 0,
+        x_position: 140,
+        y_position: 392
+      }
+    ]
+  },
+  {
+    name: "Surface",
+    index: 1,
+    preset_coordinates: [
+      {
+        room: 2,
+        screen: 1,
+        x_position: 1120,
+        y_position: 72
+      }
+    ]
+  },
+  {
+    name: "Mausoleum of the Giants",
+    index: 2,
+    preset_coordinates: [
+      {
+        room: 5,
+        screen: 0,
+        x_position: 160,
+        y_position: 312
+      }
+    ]
+  },
+  {
+    name: "Temple of the Sun",
+    index: 3,
+    preset_coordinates: [
+      {
+        room: 0,
+        screen: 1,
+        x_position: 1200,
+        y_position: 152
+      }
+    ]
+  },
+  {
+    name: "Spring in the Sky",
+    index: 4,
+    preset_coordinates: [
+      {
+        room: 6,
+        screen: 0,
+        x_position: 120,
+        y_position: 152
+      }
+    ]
+  },
+  {
+    name: "Inferno Cavern",
+    index: 5,
+    preset_coordinates: [
+      {
+        room: 6,
+        screen: 0,
+        x_position: 300,
+        y_position: 72
+      }
+    ]
+  },
+  {
+    name: "Chamber of Extinction",
+    index: 6,
+    preset_coordinates: [
+      {
+        room: 6,
+        screen: 1,
+        x_position: 40,
+        y_position: 632
+      }
+    ]
+  },
+  {
+    name: "Twin Labyrinths",
+    index: 7,
+    preset_coordinates: [
+      {
+        name: 'Twin Labyrinths (Front)',
+        room: 0,
+        screen: 1,
+        x_position: 740,
+        y_position: 392
+      },
+      {
+        name: 'Twin Labyrinths (Back)',
+        room: 15,
+        screen: 1,
+        x_position: 820,
+        y_position: 312
+      }
+    ]
+  },
+  {
+    name: "Endless Corridor",
+    index: 8,
+    preset_coordinates: [
+      {
+        room: 1,
+        screen: 0,
+        x_position: 440,
+        y_position: 72
+      }
+    ]
+  },
+  {
+    name: "Shrine of the Mother",
+    index: 9,
+    preset_coordinates: [
+      {
+        room: 7,
+        screen: 0,
+        x_position: 420,
+        y_position: 72
+      }
+    ]
+  },
+  {
+    name: "Gate of Illusion",
+    index: 10,
+    preset_coordinates: [
+      {
+        room: 4,
+        screen: 0,
+        x_position: 300,
+        y_position: 72
+      }
+    ]
+  },
+  {
+    name: "Graveyard of the Giants",
+    index: 11,
+    preset_coordinates: [
+      {
+        room: 2,
+        screen: 1,
+        x_position: 800,
+        y_position: 152
+      }
+    ]
+  },
+  {
+    name: "Temple of Moonlight",
+    index: 12,
+    preset_coordinates: [
+      {
+        room: 1,
+        screen: 1,
+        x_position: 220,
+        y_position: 552
+      }
+    ]
+  },
+  {
+    name: "Tower of the Goddess",
+    index: 13,
+    preset_coordinates: [
+      {
+        room: 7,
+        screen: 1,
+        x_position: 940,
+        y_position: 232
+      }
+    ]
+  },
+  {
+    name: "Tower of Ruin",
+    index: 14,
+    preset_coordinates: [
+      {
+        room: 6,
+        screen: 0,
+        x_position: 140,
+        y_position: 392
+      }
+      # Add Top of Ruins?
+    ]
+  },
+  {
+    name: "Chamber of Birth (Backside Door)",
+    index: 15
+  },
+  {
+    name: "Chamber of Birth (Main)",
+    index: 16,
+    preset_coordinates: [
+      {
+        room: 0,
+        screen: 0,
+        x_position: 200,
+        y_position: 392
+      }
+    ]
+  },
+  {
+    name: "Dimensional Corridor",
+    index: 17,
+    preset_coordinates: [
+      {
+        room: 0,
+        screen: 0,
+        x_position: 120,
+        y_position: 72
+      }
+    ]
+  },
+  {
+    name: "True Shrine of the Mother",
+    index: 18,
+    preset_coordinates: [
+      {
+        room: 7,
+        screen: 0,
+        x_position: 420,
+        y_position: 72
+      }
+    ]
+  },
+  {
+    name: "Gate of Time - Mausoleum",
+    index: 19
+  },
+  {
+    name: "Gate of Time - Gate of Guidance",
+    index: 20
+  },
+  {
+    name: "Gate of Time - Surface",
+    index: 21
+  },
+  {
+    name: "Surface (Night)",
+    index: 22
+  },
+  {
+    name: "Hell Temple 1",
+    index: 23,
+    preset_coordinates: [
+      {
+        room: 0,
+        screen: 0,
+        x_position: 300,
+        y_position: 72
+      }
+    ]
+  },
+  {
+    name: "Hell Temple 2",
+    index: 24,
+    preset_coordinates: [
+      {
+        room: 2,
+        screen: 0,
+        x_position: 300,
+        y_position: 72
+      }
+      # Add End of Hell Temple?
+    ]
+  },
+  {
+    name: "Burning Cavern",
+    index: 25
+  }
+]
