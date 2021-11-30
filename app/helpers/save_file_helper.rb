1264356cc7432828f1b186557a5dabbdd062314d
Justin Bantuelle <justin@mudbugmedia.com>
2021-11-30 11:12:15 -0600

    Reformat Flags and Inventory Presentation

--- app/helpers/save_file_helper.rb
+++ app/helpers/save_file_helper.rb
@@ -7,21 +7,4 @@ module SaveFileHelper
     "#{hours}h #{minutes}m #{seconds}.#{milliseconds.to_s.rjust(3, '0')}s"
   end
 
-  def save_file_tooltip(setting)
-    %Q{
-      <strong>#{setting[:name]}</strong>
-      <br />
-      #{setting_explanation(setting)}
-    }
-  end
-
-  def setting_explanation(setting)
-    if setting[:options].present?
-      setting[:options].map{ |option|
-        "<small>#{option[0]} - #{option[1]}</small>"
-      }.join('<br />')
-    elsif setting[:range].present?
-      "<small>Between #{setting[:range].min} & #{setting[:range].max}</small>"
-    end
-  end
 end
