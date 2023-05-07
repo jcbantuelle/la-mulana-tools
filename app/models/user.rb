386a5f04bb0ec9e3b266e38c05530793bcfa3cb7
Justin Bantuelle <justin@mudbugmedia.com>
2023-05-06 21:52:59 -0500

    Add Devise for Login Support

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :timeoutable, and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :trackable, :confirmable, :lockable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root controller: 'save_file', action: 'edit'
+  devise_for :users
 
   get 'save_file/edit'
   post 'save_file/update'
