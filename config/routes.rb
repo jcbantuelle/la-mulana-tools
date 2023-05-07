a1203bbbbf51822bbb74bc3388930e103207c033
Justin Bantuelle <justin@mudbugmedia.com>
2023-05-06 21:53:30 -0500

    Add Homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root controller: 'save_file', action: 'edit'
   devise_for :users
+  root controller: 'home', action: 'index'
 
   get 'save_file/edit'
   post 'save_file/update'
