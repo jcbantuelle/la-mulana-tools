c3f90d1ddfac03d4bb138ece36741abf9d53e844
Justin Bantuelle <justin@mudbugmedia.com>
2023-05-14 18:22:42 -0500

    Clean Up User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable, :confirmable, :lockable
+
   has_and_belongs_to_many :games
 end
