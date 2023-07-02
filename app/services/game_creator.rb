a4b4ebe67ec0b5e00f590ab0e07a0c53c2c80f33
Justin Bantuelle <justin@mudbugmedia.com>
2023-07-01 22:28:44 -0500

    Switch Game Creation To User Player Association

--- app/services/game_creator.rb
+++ app/services/game_creator.rb
@@ -5,14 +5,14 @@ class GameCreator
   attr_reader :players
 
   def initialize(user_ids)
-    @players = user_ids.map { |user_id|
-      User.find(user_id)
-    }
+    @players = user_ids.map(&:to_i).each_with_index.map do |user_id, index|
+      Player.new(user_id: user_id, player_id: index+1)
+    end
   end
 
   def create
     generate_item_placement
-    @game = Game.new(users: @players, state: @game_state, uid: Digest::SHA512.hexdigest(@game_state.to_s))
+    @game = Game.new(players: @players, state: @game_state, uid: Digest::SHA512.hexdigest(@game_state.to_s))
     @game.save
     @game
   end
@@ -25,15 +25,15 @@ class GameCreator
 
   def generate_item_placement
     unshuffled_items = []
-    @players.length.times do |i|
+    @players.each do |player|
       unshuffled_items += Items::IDS.map{ |item_id|
-        [item_id, i]
+        [item_id, player.player_id]
       }
     end
     shuffled_items = unshuffled_items.shuffle
     @game_state = {}
-    @players.length.times do |i|
-      @game_state[i] = unshuffled_items.slice!(0, Items::IDS.length)
+    @players.each do |player|
+      @game_state[player.player_id] = unshuffled_items.slice!(0, Items::IDS.length)
     end
   end
 end
