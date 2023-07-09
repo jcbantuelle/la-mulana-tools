ce444527229d790ae16569365011c35f4182a8eb
Justin Bantuelle <justin@mudbugmedia.com>
2023-07-08 21:43:40 -0500

    Add Config File To Game Zip

--- app/services/game_downloader.rb
+++ app/services/game_downloader.rb
@@ -1,4 +1,5 @@
 require 'zip'
+require 'perfect_toml'
 
 class GameDownloader
 
@@ -10,7 +11,25 @@ class GameDownloader
   def download
     Zip::File.open_buffer('') { |f|
       f.get_output_stream("script.rcd") { |f| f.puts "An RCD File" }
-      f.get_output_stream("lamulana-config.toml") { |f| f.puts "A config file" }
+      f.get_output_stream("lamulana-config.toml") { |f| f.puts config_file }
     }.string
+  end
+
+  private
+
+  def config_file
+    PerfectTOML.generate({
+      log_file_name: 'lamulanamw.txt',
+      game_uid: @game.uid,
+      server_url: ENV['WEBSOCKET_URL'],
+      user_id: @player.player_id,
+      players: players
+    })
+  end
+
+  def players
+    @game.players.map { |player|
+      [player.player_id, player.user.username]
+    }.to_h
   end
 end
