fd7746e7ec9efe106bc74465b1e07505934d52b9
Justin Bantuelle <justin@mudbugmedia.com>
2023-07-08 20:46:09 -0500

    Remove Chest Entries For Contents That Aren’t Randomized Yet

--- app/value_objects/chests.rb
+++ app/value_objects/chests.rb
@@ -1,75 +1,42 @@
 class Chests
   FLAGS = [
     259, # Guidance, Treasures
-    199, # Guidance, Sacred Orb
     171, # Guidance, Crucifix
-    210, # Guidance, Map
     169, # Guidance, Grail
     229, # Guidance, yagostr.exe
-    210, # Guidance, Map
-    142, # Guidance, Ankh Jewel
     182, # Surface, Feather
     167, # Surface, Shell Horn
     195, # Surface, Birth Seal (2)
-    200, # Surface, Sacred Orb
-    211, # Mausoleum, Map
-    143, # Mausoleum, Ankh Jewel
-    201, # Mausoleum, Sacred Orb
-    212, # Temple of the Sun, Map
     170, # Temple of the Sun, Isis Pendant
-    202, # Temple of the Sun, Sacred Orb
     174, # Temple of the Sun, Bronze Mirror
-    144, # Temple of the Sun, Ankh Jewel
-    203, # Spring, Sacred Orb
     168, # Spring, Glove
-    213, # Spring, Map
     177, # Spring, Scalesphere
     194, # Spring, Origin Seal (1)
-    145, # Spring, Ankh Jewel
-    214, # Inferno, Map
     193, # Inferno, Ice Cape
-    215, # Extinction, Map
-    204, # Extinction, Sacred Orb
     196, # Extinction, Life Seal (3)
-    216, # Twin Labyrinth, Map
-    205, # Twin Labyrinth, Sacred Orb
-    148, # Twin Labyrinth, Ankh Jewel
-    217, # Endless Corridor, Map
     188, # Endless Corridor, Twin Statue
     158, # Shrine of the Mother, Crystal Skull
-    206, # Shrine of the Mother, Sacred Orb
     260, # Shrine of the Mother, Diary
     242, # Shrine of the Mother, bounce.exe
     197, # Shrine of the Mother, Death Seal (4)
-    218, # Shrine of the Mother, Map
     154, # Illusion, Cog of the Soul
     162, # Illusion, Key of Eternity
     184, # Illusion, Fairy Clothes
-    219, # Illusion, Map
-    220, # Graveyard, Map
     178, # Graveyard, Gauntlet
     244, # Graveyard, mirai.exe
     173, # Moonlight, Grapple Claw
-    221, # Moonlight, Map
     187, # Moonlight, Fruit of Eden
     163, # Moonlight, Serpent Staff
-    222, # Goddess, Map
     175, # Goddess, Eye of Truth
     180, # Goddess, Plane Model
     191, # Goddess, Spaulder
-    207, # Ruin, Sacred Orb
-    146, # Ruin, Ankh Jewel
-    223, # Ruin, Map
     151, # Ruin, Djed Pillar
     159, # Ruin, Vessel
     190, # Birth, Perfume
     161, # Birth, Woman Statue
     192, # Birth, Dimensional Key
     156, # Birth, Pochette Key
-    225, # Dimensional Corridor, Map
-    149, # Dimensional Corridor, Ankh Jewel
     153, # Dimensional Corridor, Magatama Jewel
-    208, # Dimensional Corridor, Sacred Orb
     245  # Gate of Time Surface, lamulana.exe
   ]
 end
