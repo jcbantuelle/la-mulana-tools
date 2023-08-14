32fd654b3d1feb2e197db22b5725eaf5ba60a3ac
Justin Bantuelle <justin@mudbugmedia.com>
2023-08-13 20:39:43 -0500

    Add Guidance Transitions

---
+++ app/value_objects/locations/guidance.rb
@@ -0,0 +1,35 @@
+module Location::Guidance
+
+  GUIDANCE_MAIN = {
+    connections: [
+      {
+        surface_main: {},
+        spring_main: {},
+        sun_top: {},
+        mausoleum: {}
+      }
+    ],
+    events: [{}],
+    randomized_contents: [{}]
+  }
+
+  GUIDANCE_DOOR = {
+    connections: [
+      {
+        guidance_main: {},
+        illusion_grail: {
+          requirements: ->(world) {
+            world.checks[:bronze_mirror] && world.checks[:amphisbaena_defeated]
+          }
+        }
+      }
+    ],
+    events: [{}],
+    randomized_contents: [{}]
+  }
+
+  NODES = {
+    guidance_main: GUIDANCE_MAIN,
+    guidance_door: GUIDANCE_DOOR
+  }
+end
