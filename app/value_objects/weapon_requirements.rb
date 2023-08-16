e303b6e3e64e2d20367ce5bb96ed08cca1437695
Justin Bantuelle <justin@mudbugmedia.com>
2023-08-16 09:11:51 -0500

    Implement Weapon Requirements

---
+++ app/value_objects/weapon_requirements.rb
@@ -0,0 +1,33 @@
+class WeaponRequirements
+
+  def self.main_attack(world)
+    world.checks[:whip] ||
+    world.checks[:chain_whip] ||
+    world.checks[:flail_whip] ||
+    world.checks[:keysword] ||
+    world.checks[:knife] ||
+    world.checks[:katana] ||
+    world.checks[:axe]
+  end
+
+  def self.far_attack(world)
+    world.checks[:whip] ||
+    world.checks[:chain_whip] ||
+    world.checks[:flail_whip] ||
+    world.checks[:keysword] ||
+    world.checks[:axe]
+  end
+
+  def self.subweapon(world)
+    world.checks[:shuriken] ||
+    world.checks[:rolling_shuriken] ||
+    world.checks[:rolling_shuriken] ||
+    world.checks[:earth_spear] ||
+    world.checks[:flare_gun] ||
+    world.checks[:bomb] ||
+    world.checks[:chakram] ||
+    world.checks[:caltrops] ||
+    world.checks[:pistol]
+  end
+
+end
